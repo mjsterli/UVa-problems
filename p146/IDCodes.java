5d25887d332425327c53066fae8c11b33f5aab63
Curtis Love <clove@callisto.hamptonbrook.int>
2016-12-22 22:07:47 -0600

    Added PrintNextCode function p1

--- p146/IDCodes.java
+++ p146/IDCodes.java
@@ -17,8 +17,32 @@ public class IDCodes
         System.out.println(s);
       }
     }
-
     catch( IOException e )
     {}
+
+    Iterator<String> iterator = list.iterator();
+    while( iterator.hasNext())
+      System.out.println( PrintNextCode(iterator.next()));
+
+  }
+
+  public static String PrintNextCode( String code )
+  {
+    int i = code.length()-1, j = code.length() - 2;
+    boolean found = false;
+
+    while( !found && j >= 0 )
+    {
+      if( code[j] < code[i] )
+      {
+        code[i] = (char)((int)code[i] + (int)code[j]);
+        code[j] = (char)((int)code[i] - (int)code[j]);
+        code[i] = (char)((int)code[i] - (int)code[j]);
+        found = true;
+      }
+
+      i--;
+      j--;
+    }
   }
 }
