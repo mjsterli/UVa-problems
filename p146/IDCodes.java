38c9408d030ad96291d0bda277661d8a15c98a5d
Curtis Love <clove@callisto.hamptonbrook.int>
2016-12-22 20:25:54 -0600

    Simply read in and output lines

--- p146/IDCodes.java
+++ p146/IDCodes.java
@@ -1,7 +1,24 @@
+import java.util.*;
+import java.io.*;
+
 public class IDCodes
 {
   public static void main( String[] args )
   {
-    System.out.println( "this is a test");
+    String s;
+    List<String> list = new ArrayList<String>();
+    BufferedReader input = new BufferedReader( new InputStreamReader( System.in ));
+
+    try
+    {
+      while(( s = input.readLine() ) != null && s.indexOf("#") == -1 )
+      {
+        list.add(s);
+        System.out.println(s);
+      }
+    }
+
+    catch( IOException e )
+    {}
   }
 }
