7fd086c7fe27ee9c15261e1dbc5229c429785556
Curtis Love <clove@callisto.hamptonbrook.int>
2017-01-25 23:02:30 -0600

    v1.0 - Runtime error
    Need to check all cases where this maybe failing.

--- p101/BlockMoves.cpp
+++ p101/BlockMoves.cpp
@@ -4,37 +4,96 @@
 
 using namespace std;
 
+void Print( vector< vector<int> > blocks )
+{
+  for( int i = 0; i < blocks.size(); i++ )
+  {
+    cout<<i<<": ";
+    for( int j = 0; j < blocks[i].size(); j++ )
+      if( blocks[i][j] >= 0 )
+        cout<<blocks[i][j]<<" ";
+    cout<<endl;
+  }
+  cout<<endl;
+}
+
+int GetStackIn( vector< vector<int> > blocks, int number )
+{
+  return ( blocks[number][0] >= 0 ? number : abs(blocks[number][0]) );
+}
+
 void Initialize( vector< vector<int> > &blocks, int number )
 {
-  vector<int> stack = blocks[abs(blocks[number][0])];
-  do
+  int popped;
+  vector<int> *stack = &blocks[GetStackIn( blocks, number)];
+
+  while( stack->back() != number )
   {
-    int popped = stack.pop_back();
+    popped = stack->back();
+    stack->pop_back();
     blocks[popped][0] = popped;
-  }while( stack.back() != number );
+  }
 }
 
 void Move( vector< vector<int> > &blocks, int from, int to )
 {
-  vector<int> tempStack, fromStack, toStack;
+  int popped;
+  vector<int> tempStack, *fromStack, *toStack;
   bool found = false;
-  fromStack = blocks[abs(blocks[from][0])];
 
+  fromStack = &blocks[GetStackIn(blocks, from)];
+  if( fromStack->size() == 1 )
+    tempStack.push_back( fromStack->back() );
+  else
+  {
+    do
+    {
+      popped = fromStack->back();
+      if( fromStack->size() > 1 )
+        fromStack->pop_back();
+      tempStack.push_back( popped );
+    }while( popped != from );
+  }
+
+  toStack = &blocks[GetStackIn(blocks, to)];
   do
   {
-    found = ( fromStack.back() == from );
-    tempStack.push_back( fromStack.pop_back() );
-  } while( !found );
-
-  toStack = blocks[abs(blocks[to][0])];
-  do
-  {
-    blocks[tempStack.back()][0] = to * -1;
-    toStack.push_back( tempStack.pop_back() );
+    blocks[tempStack.back()][0] = GetStackIn(blocks, to) * -1;
+    popped = tempStack.back();
+    toStack->push_back( popped );
+    tempStack.pop_back();
   }while( tempStack.size() != 0 );
 }
 
+
 int main()
 {
+  vector< vector<int> > blocks;
+  int numOfBlocks, a, b;
+  string actionA = "", actionB;
 
+  cin>>numOfBlocks;
+  for( int i = 0; i < numOfBlocks; i++ )
+  {
+    blocks.push_back( vector<int>() );
+    blocks[i].push_back(i);
+  }
+
+  cin>>actionA;
+  while( actionA != "quit" )
+  {
+    cin>>a>>actionB>>b;
+    if( a != b && a < numOfBlocks && b < numOfBlocks && GetStackIn( blocks, a ) != GetStackIn( blocks, b ) )
+    {
+      if( actionA == "move" )
+        Initialize( blocks, a );
+
+      if( actionB == "onto" )
+        Initialize( blocks, b );
+
+      Move( blocks, a, b );
+    }
+    cin>>actionA;
+  }
+  Print( blocks );
 }
